840d226eeb8c3794c7d823a81c32f75c1718d0a5
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-09 13:53:43 -0500

    buttons appended to DOM and click handlers added

--- scripts/client.js
+++ scripts/client.js
@@ -10,12 +10,24 @@ function main(){
 
 function addClickHandlers(){
   $('#generateBtn').on('click',addDiv);
+  $('#output').on('click','.swapBtn',swapColors);
+  $('#output').on('click','.deleteBtn',deleteDiv);
 }
 
 function addDiv(){
   count++;
-  var $div = $('<div></div>');
+  var $div = $('<div class="generatedDiv"></div>');
   var $counter = $('<p>'+ count +'</p>');
-  $div.append($counter);
+  var $swapBtn = $('<button class="swapBtn">Swap</button>');
+  var $deleteBtn = $('<button class="deleteBtn">Delete</button>');
+  $div.append($counter,$swapBtn,$deleteBtn);
   $('#output').append($div);
+}
+
+function swapColors() {
+  console.log(this);
+}
+
+function deleteDiv() {
+  console.log(this);
 }
